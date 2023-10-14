6e3f9eae09844b5820f8767c82d95fd31f5e4ade
CurryBaxter <Felix19872@web.de>
2023-10-14 16:02:25 +0200

    testing out generics

---
+++ src/Kapitel13/Arena.java
@@ -0,0 +1,19 @@
+package Kapitel13;
+
+import Kapitel11.Tier;
+
+public class Arena <T extends Tier, V extends Tier> {
+
+    private T Animal1;
+    private V Animal2;
+
+    public Arena (T anml1, V anml2){
+        Animal1 = anml1;
+        Animal2 = anml2;
+    }
+
+    public void showdown(){
+        // IF Schleife zum ausführen von eat() und ausgabe von überlebenden Tier
+        if ()
+    }
+}
